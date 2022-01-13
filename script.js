da18de58954f94bcb260071a493b465c2f9a8c91
Stephen Delgado <devang0gh@gmail.com>
2022-01-13 13:19:24 -0600

    initial proj

--- script.js
+++ script.js
@@ -1,190 +1,148 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+//We need to target these variables once their respective elements are added to the HTML
+var locationInput = "Austin"   //string, can be zip code, city, or current address
+var priceRangeInput = "2" //1 = $, 2 = $$, 3 = $$$, 4 = $$$$
+var distanceInput = 5  //in meters max is 40000 meters 
+var foodTypeInput = "sit-down italian"  //fastfood-sitdown-
+var timeInput = '&open_now=true'    //now, breakfast,lunch,dinner //if user selects open now, then add '&open_now=true' to query string, if user selects time then add '&open_at=[time(int)]
+var distanceInputMeters 
+var searchResultsArray = []   //will store all parsed data from Yelp API pull
 
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
-var timer;
-var timerCount;
 
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
-
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
-
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
+function lengthConverter() {
+  // distanceInput = distanceInput.val()
+  distanceInputMeters = (distanceInput/0.00062137).toFixed(0).toString();
+  console.log (typeof(distanceInputMeters)) //converts miles to meters from user input
 }
+lengthConverter()
 
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
-}
+var userLocationLatLng        //will store coordinates of locationInput
 
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
+// geoLocation()
+// function geoLocation() {  //google API call to convert address/city/zipcode/etc to coordinates
+//   fetch('https://maps.googleapis.com/maps/api/geocode/json?address='+locationInput+'&key=AIzaSyC2zgWJRoeij-FPFj_I39eZ9oDHPcXlQoc')
+// .then(function(response) {
+//   return response.json();
+// }).then(function(data) {
+//   console.log("-------Google Geolocation Call-------");
+//   console.log(data);
+//   userLocationLatLng = data.results[0].geometry.location;
+//   console.log("Below are the coordinates of the user's location input");
+//   console.log(userLocationLatLng);
+// })
+// }
 
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
+yelpCallAPI()
+function yelpCallAPI() {
+//the first half of this url is needed because of a CORS error
+var yelpUrl = 'https://salty-mountain-68764.herokuapp.com/https://api.yelp.com/v3/businesses/search?radius='+ distanceInputMeters +'&price='+ priceRangeInput +'&term='+foodTypeInput+'&location=' + locationInput + timeInput
+console.log (yelpUrl)
+var APIKEY = 'Dm03wv4YdEsLaKufEPshYjbDKuUxpKY621FUmPuz_y172PgIO3devn-UJtkkEPc6O7WuSgsyAc9PZOsA1kySWKeAb3mZb41NPezvv9taNTuHaSeuDkWNqrUI8KfbYXYx'
+var Bearer = 'Bearer ' + APIKEY   //needed for authentication header
 
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
+  fetch(yelpUrl, {
+    headers: {
+      Authorization: Bearer
+    }
+  }).then(function(response) {
+    return response.json()      //converts data into JSON
+  }).then(function(data) {
+    console.log("-------Yelp Call-------");
+    console.log(data);    //displays yelp call Data
+      for (var i = 0; i < 5; i++) {     //for loop iterates through data object and parses from the first 5 elements in the array
+      var searchResult = {
+        restaurantName: data.businesses[i].name,
+        price: data.businesses[i].price,
+        location: [data.businesses[i].coordinates.longitude, data.businesses[i].coordinates.latitude],
+        rating: data.businesses[i].rating,
+        image: data.businesses[i].image_url,
+        websiteLink: data.businesses[i].url,
       }
+      searchResultsArray.push(searchResult);  //pushes searchResult (parsed data object) into array before iterating to next index in data array
     }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
+    console.log("Below are search results that will be appended to the page")
+    console.log(searchResultsArray);
+    // displayMap()
+  })
 }
 
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
+//Responsible for displaying map on results.html and fav.html
+// function displayMap() {
+// let map;
+// const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" //needed for marker labels
 
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
+// initMap()
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: userLocationLatLng, //determines center of map (user location)
+//     zoom: 10,
+//     }
+//   );
+//   var marker
+//   for (var i = 0; i<searchResultsArray.length; i++) {  //for loop to iterate through searchResultsArray
+//     console.log("Adding '"+labels.charAt(i)+ "' Marker to map with coordinates: ");
+//     console.log(searchResultsArray[i].location);
+//     var LatLng = {
+//       lat: searchResultsArray[i].location[1], //pulls latitude from searchResultsArray
+//       lng: searchResultsArray[i].location[0], //pulls longitude from searchResultsArray
+//     };
+//     var contentString = searchResultsArray[i].restaurantName
+//     console.log(contentString);
 
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
+//     var marker = new google.maps.Marker({  //places marker
+//       position: LatLng,
+//       label: labels.charAt(i),
+//       map,
+//       title: searchResultsArray[i].restaurantName,
+//     });
 
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
+//     //displays window that details restaurant name if marker is clicked
+//     //based off code from https://stackoverflow.com/questions/11106671/google-maps-api-multiple-markers-with-infowindows
+//     var content = searchResultsArray[i].restaurantName
+//     var infoWindow = new google.maps.InfoWindow()
+//     google.maps.event.addListener(marker,'click', (function(marker,content,infoWindow) {
+//       return function() {
+//         infoWindow.setContent(content)
+//         infoWindow.open(map,marker);
+//       };
+//     })(marker,content,infoWindow))
+// }
+// }
+// }
 
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
+//SCRIPT FILE 1
+//Target specific elements on our form to recieve values inputted the user
+  // var locationInput 
+  // var priceRangeInput
+  // var distanceInput 
+//user input miles, needs to be converted to meters (There is a max look up in DOCS). Currently lines 10 thru 15.SD.
+  // var foodTypeInput
+  // var timeInput   
+    //Pulls values when the user press submit
+      //Event listenea
+        //redirect the user to results.html
 
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
+// Consider for later--user inputs how many search results they would like to see
 
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
+//SCRIPT FILE 2
+  //Input variables need to be placed into query string for Yelp API
+  //Fetch and convert data to JSON from YELP API
+  //Pull variables from JSON and put them into an object.
+    //use for loop to iterate through 5 different businesses        <--Functional, but maybe should pull more for improved user experience.  Consider this after we get main functionality and adding option to scroll more
+      //var searchResult = {
+      //   title: ,
+      //   price: ,
+      //   location: [latitude, longitude]
+      //   star-rating: ,
+      //   hours: ,
+      // } 
+      //push 5 searchResults into an array --> var searchResultsArray
 
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
+//append all our options to left column on results page using Dynamic HTML
 
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
 
-// Calls init() so that it fires when page opened
-init();
-
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
-
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
+  //using pull location in searchResult object, we need to display a pin for that location.  Each pin will be labeled with a number corresponding to that search results location.
+  //all 5 pins will be on the map at the same time.
+    //for loop to iterate through searchResultsArray.location to display all markers at the same time.
+  
+  
+  //We need to convert locationInput to latitude and longitude using either Google Maps API (You will need to look up how to do this on docs) or you can use OpenWeatherMap or another API.
